14c56f9d10dcdb3a9be9e58d77454e45ef8a6537
punch-bite <griffynpunch@gmail.com>
2026-08-02 21:04:52 +0200

    codemagic 5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,12 +4,24 @@ plugins {
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
-
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.20")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
+    implementation("com.google.firebase:firebase-analytics")
+}
 android {
     namespace = "com.noi.noi_ohada_invoice_pro"
     compileSdk = 36
-    ndkVersion = "28.2.13676358"
-
+    ndkVersion = "28.2.13676358" 
     defaultConfig {
         applicationId = "com.noi.noi_ohada_invoice_pro"
         minSdk = flutter.minSdkVersion
@@ -17,23 +29,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
-
     // Fixed: Moved inside the android block and updated syntax
     kotlinOptions {
         jvmTarget = "17"
     }
-
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
