37a0faacf2179cbba612fa3bf1abcc38228b1212
punch-bite <griffynpunch@gmail.com>
2026-09-06 18:20:08 +0200

    modfication de build gradle kootling

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -88,7 +88,7 @@ android {
     }
     kotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
         }
     }
 
