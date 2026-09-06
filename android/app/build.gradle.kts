031e9942e5ad6f6fa1d355a25f7b8b466c394383
punch-bite <griffynpunch@gmail.com>
2026-09-06 17:57:10 +0200

    modfication de build gradle kootling

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -81,8 +81,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
         // ✅ Requis par flutter_local_notifications (cf. dependencies).
         isCoreLibraryDesugaringEnabled = true
     }
